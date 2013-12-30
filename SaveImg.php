34052da4267005870d1be5b3f6df0ca514dd846d
DingHui <dhjy123456@163.com>
2013-12-30 14:34:26 +0800

    整理文件上传应用

--- SaveImg.php
+++ SaveImg.php
@@ -4,11 +4,11 @@
 	include("TopClient.php");
 	include("PictureUploadRequest.php");
 	
-	echo $_POST['filename']."<br>";
-	
+	echo $_POST['filename']."<br />";
+	//请注意修改appkey、secret、sessionkey为自己的内容
 	$appkey = "21686863";
 	$secret = "126b3f5051a4dde3ed050f620790bff5";
-	$sessionKey = "6101002af818ac97b4cb658d89cb8fb1e51e3dd066937c8263664221";
+	$sessionKey = "6100b27eac2d1c1b5071a54aa4033dcf0b713cb30aaa4ed263664221";
 
 
 	$c = new TopClient;
@@ -18,6 +18,7 @@
 	$req = new PictureUploadRequest;
 	$req->setPictureCategoryId(0);
 	//附件上传的机制参见PHP CURL文档，在文件路径前加@符号即可
+	//请注意setImg参数的书写方式
 	$req->setImg("@_FILES['userfile']");
 	$req->setImageInputTitle("Test.jpg");
 	$req->setTitle("TestTitle");
--- UploadImg.php
+++ UploadImg.php
@@ -3,18 +3,14 @@
 <meta http-equiv="Content-Type" content="text/html; Charset=UTF-8">
 </head>
 <body>
-<h1>上传图片</h1>
+	<h1>上传图片</h1>
 	<form enctype="multipart/form-data" action="/SaveImg.php" method="POST">
 		<!-- MAX_FILE_SIZE must precede the file input field -->
 		<input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
 		<!-- Name of input element determines name in $_FILES array -->
-		Send this file: <input name="userfile" type="file" />
-		filename:<input name="filename" type="text" />
+		请选择文件: <input name="userfile" type="file" />
+		文件名称:<input name="filename" type="text" />
 		<input type="submit" value="Send File" />
-	</form>
-	<form action="down.php" method="POST">
-		<input name="filename" type="text" />
-		<input type="submit" value="Get File" />
 	</form>
 </body>
 </html>
